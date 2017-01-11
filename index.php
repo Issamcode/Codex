bc538cbdd0df3778f8ed71c296e25d6817a2b837
Issamcode <haidaouiissam@gmail.com>
2017-01-11 13:44:21 +0100

    Add files via upload

---
+++ index.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<meta charset="utf-8">
+		<title>Home</title>
+		<script src="src/index.js"></script>
+		<link href="https://fonts.googleapis.com/css?family=Bungee+Shade" rel="stylesheet">
+		<link href="https://fonts.googleapis.com/css?family=Bree+Serif" rel="stylesheet">
+		<link href="https://fonts.googleapis.com/css?family=Russo+One" rel="stylesheet">
+		<link href="https://fonts.googleapis.com/css?family=Comfortaa" rel="stylesheet">
+		<link rel="stylesheet" href="src/style.css" />
+	</head>
+	<body>
+		<div class="main">
+			<div class="home">
+				<div class="nav">
+					<ul>
+						<li id="header"><a href="#">CodeX</a></li>
+						<li><a href='#'>Social Media</a></li>
+						<li><a href='#'>About Me</a></li>
+						<li><a href='#'>Our App</a></li>
+					</ul>
+				</div>
+				<div class="content">
+					<div class="content-header">
+						<h3>WE LOVE TO  </h3>
+						<h1>WE CREATE WEBAPP AND WEBSITE FOR COMPANYS AND CLIENT</h1>
+					</div>
+					<div class="content-body">
+						<p>
+							Codex Dev creates webapps and website ,to grow businesses and make
+							people's lives better.
+						</p>
+					</div>
+				</div>
+			</div>
+		</div>
+	</body>
+</html>
